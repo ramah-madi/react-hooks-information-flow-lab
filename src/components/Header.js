c3455f3b3d4e85444f89de476ae627d611c137bd
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-25 16:54:29 +0300

    Completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Header({mode, onDarkModeClick}){
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          { mode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    )
+        
+}
+
+export default Header
